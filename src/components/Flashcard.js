443af1fd5b373c2aa22a30596dac556489caa398
TimothyAnggara <timothyanggara@gmail.com>
2023-02-24 17:48:35 +1100

    Added Flashcard

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -7,6 +7,7 @@ export default function Flashcard(props){
     }
     return(
         <button className="mt-16 block mx-auto max-w-sm p-16 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" onClick={handleOnClick}> {/* Div for Entire Card */}
+            <span></span>
             <div>  {/* Div for question */}
                 <h1 class="flex text-center justify-center">{content}</h1>
             </div>
