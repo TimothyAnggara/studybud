b59a96e3483f98b4f802589dda293fa302126430
TimothyAnggara <timothyanggara@gmail.com>
2023-02-24 17:46:42 +1100

    Worked on the Flashcard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Flashcard from "./components/Flashcard.js";
+import react, { useState } from "react";
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Flashcard />
     </div>
   );
-}
+  }
 
+const SAMPLE_FLASHCARDS = [{
+  id : 1,
+  question: "What is a stack data structure?",
+  answer: "An array where we can only access, pop, and see the top of the stack",
+  options: ["1", "2", "3", "4"]
+}, {
+  id: 2,
+  question: "What is set data structure?",
+  answer: "IDK",
+  options: ["1","2","3","4"]
+}
+]
 export default App;
---
+++ src/components/Flashcard.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Flashcard(props){
+    const [content, setContent] = useState("What's 9+10?")
+    function handleOnClick(){
+        setContent("you stupid");
+    }
+    return(
+        <button className="mt-16 block mx-auto max-w-sm p-16 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" onClick={handleOnClick}> {/* Div for Entire Card */}
+            <div>  {/* Div for question */}
+                <h1 class="flex text-center justify-center">{content}</h1>
+            </div>
+        </button>
+    )
+}
